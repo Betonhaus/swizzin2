879056e301bc2a204ef0eef125a7ec5254416bc4
Betonhaus <149969033+Betonhaus@users.noreply.github.com>
2023-11-05 15:21:05 -0700

    Update instancefind.sh

--- instancefind.sh
+++ instancefind.sh
@@ -6,9 +6,13 @@ echo "Reading list of arr instances"
 x = 0
 for files in /opt/swizzin/core/custom/*
 do
+echo "add to list"
   list[$x] = "$files"
+  echo "run sed"
   sed -n 's/^ *pretty_name *= *//p' /opt/swizzin/core/custom/$files
+  echo "print fi8lename"
   echo "$files"
-  x++
+  Echo "increment"
+  $x++
 done
-print $x
+print "x is $x"
