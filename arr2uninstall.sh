4d905376e64fb60345cf54354be53c92cac5c456
Betonhaus <149969033+Betonhaus@users.noreply.github.com>
2023-11-05 14:17:05 -0700

    Update and rename sonarr2uninstall.sh to arr2uninstall.sh

--- arr2uninstall.sh
+++ arr2uninstall.sh
@@ -4,19 +4,48 @@
 # For Uninstalling the additional Sonarr instances
 
 # code for finding all instances and presenting as a list to choose from
+#prompt for server type
+#code for checking which servers are installed and only presenting them
+echo "Please enter which server type you wish to remove.
+1. Sonarr
+2. Radarr
+3. Readarr
+4. Lidarr
+Enter server number (1):"
+read input
+case "$input" in
+    "1") 
+          servname = "Sonarr"
+          servsysname = "sonarr";;
+    "") 
+          servname = "Sonarr"
+          servsysname = "sonarr";;
+    "2") 
+          servname = "Radarr"
+          servsysname = "radarr";;
+    "3") 
+          servname = "Readarr"
+          servsysname = "readarr";;
+    "4") 
+          servname = "Lidarr"
+          servsysname = "lidarr";;
+    *) 
+          echo "invalid input"
+          exit 1;;         
+esac
 
 #prompt for name of instance to remove
 echo "Please enter the following information of the server, ensure there anre no typos"
-echo "Proper name for server (Sonarr 4K):"
+echo "Proper name for server (${servname} 2):"
 read arrname
 if test -z "$arrname" then
-    arrname = "Sonarr 4K"
+    arrname = "${servname} 2"
 fi
 #code here to make lowercase and remove whitespace from input to present as default system name
-echo "System name for server (sonarr4k):"
+echo "System name for server (${servsysname}2):"
 read arrsysname
 if test -z "$arrsysname" then
-    arrsysname = "sonarr4k"
+    arrsysname = "${servsysname}2"
 fi
 
 
@@ -43,5 +72,5 @@ sed -e "s/    pretty_name = \"$arrname\"//g" -i /opt/swizzin/core/custom/profile
 sed -e "s/    baseurl = \"\/$arrsysname\"//g" -i /opt/swizzin/core/custom/profiles.py
 sed -e "s/    systemd = \"$arrsysname\"//g" -i /opt/swizzin/core/custom/profiles.py
 sed -e "s/    check_theD = True//g" -i /opt/swizzin/core/custom/profiles.py
-sed -e "s/    img = \"sonarr\"//g" -i /opt/swizzin/core/custom/profiles.py
-sed -e "s/class sonarr_meta(sonarr_meta)://g" -i /opt/swizzin/core/custom/profiles.py
+sed -e "s/    img = \"${servsysname}\"//g" -i /opt/swizzin/core/custom/profiles.py
+sed -e "s/class ${servsysname}_meta(${servsysname}_meta)://g" -i /opt/swizzin/core/custom/profiles.py
