29b9bb4d7ba34c6299838569a096dd0cdba71729
Betonhaus <149969033+Betonhaus@users.noreply.github.com>
2023-11-05 16:01:04 -0700

    Update instancefind.sh

--- instancefind.sh
+++ instancefind.sh
@@ -3,21 +3,22 @@
 # Script by @Betonhaus
 # code for finding all instances and presenting as a list to choose from
 echo "Reading list of arr instances"
-x=0
+x=-1
 for files in /opt/swizzin/core/custom/*
 do
 if [ $files != '/opt/swizzin/core/custom/__pycache__' ]; then
-  echo "add to list"
-  list[$x]='$files'
-  echo "run sed"
-  sed -n 's/^ *pretty_name *= *//p' $files
-  echo "print fi8lename"
-  echo "$files"
   echo "increment"
   x=$(($x + 1))
+  echo "add to list"
+  list[$x]='${files}'
+  echo "run sed"
+  sed -n 's/^ *pretty_name *= *//p' $files
+  echo "print filename"
+  echo "$files"
 else
         echo "skipping psycache"
 fi
 done
 echo "number of instances found: $x"
 echo ${list[0]}
+echo ${list[1]}
